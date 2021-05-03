cd3ac33e2f46e153504372cc225c5b4e3287d970
eddie51405 <e0932332176@yahoo.com.tw>
2021-05-03 15:02:38 +0800

    58 done

--- 58. Length of Last Word.cc
+++ 58. Length of Last Word.cc
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        //  TODO
         int size = s.size();
-        int ans = 0;
-        int tmp = 0;
+        int index[size];
         for (int i = 0; i < size; i++) {
             if (s[i] == ' ') {
-                ans = tmp;
-                tmp = 0;
+                index[i] = 1;
             } else {
-                tmp += 1;
-                if (i + 1 == size && s[i] != ' ') {
-                    ans = tmp;
-                }
+                index[i] = 0;
             }
-
+        }
+        int flag = 0;
+        int ans = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (index[i] == 0) {
+                flag = 1;
+                ans++;
+            }
+            if (flag && index[i] == 1) {
+                return ans;
+            }
         }
         return ans;
     }
