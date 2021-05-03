0de2e89bf82f943631f9dee138ba279130c429f6
eddie51405 <e0932332176@yahoo.com.tw>
2021-05-03 14:38:27 +0800

    53 Time Limit Exceeded

--- 53. Maximum Subarray.cc
+++ 53. Maximum Subarray.cc
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-
+        // Time Limit Exceeded
+        int size = nums.size();
+        int sum = -1000000;
+        for (int i = 1; i < size + 1; i++) {
+            for (int j = 0; j < size - i + 1; j++) {
+                int tmp = 0;
+                for (int k = j; k < j + i; k++) {
+                    tmp += nums[k];
+                }
+                if (tmp > sum) {
+                    sum = tmp;
+                }
+            }
+        }
+        return sum;
     }
 };
